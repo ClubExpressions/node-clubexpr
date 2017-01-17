d4a9601ef10e52efd56b597f8c900afe683249fa
Christophe Gragnic <cgragnic@netc.fr>
2017-01-17 18:14:38 +0100

    Install a (non working) dropdown

--- react/Demo.js
+++ react/Demo.js
@@ -1,5 +1,6 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
+var Select = require('react-select');
 
 var Expression = require('./Expression');
 var ClubExpr = require('../index');
@@ -10,8 +11,19 @@ module.exports = React.createClass({
       expressions: ClubExpr.expressions
     };
   },
+  _onNature: function() {
+      console.log('nature');
+  },
   render: function() {
+      var options = [
+          { value: 'All', label: 'Nature indifférente' },
+          { value: 'Somme', label: 'Sommes' },
+          { value: 'Produit', label: 'Produits' }
+      ];
       return <div>
+      <Select name="form-field-name" value="All" options={options}
+              onChange={this._onNature}
+      />
       <ul>
         {this.state.expressions.map(function(exprObj, idx){
           return <li key={idx}>
