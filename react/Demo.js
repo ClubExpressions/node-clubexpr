b0b481a1a358f14585a1aa0ccf4d2585d04781b7
Christophe Gragnic <cgragnic@netc.fr>
2017-01-17 22:00:40 +0100

    Make the 'nature' dropdown functional

--- react/Demo.js
+++ react/Demo.js
@@ -8,26 +8,52 @@ var ClubExpr = require('../index');
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      expressions: ClubExpr.expressions
+      expressions: ClubExpr.expressions.map(function (exprObj) {
+        exprObj.properties = ClubExpr.properties(exprObj.expr);
+        return exprObj;
+      }),
+      expressionsShown: ClubExpr.expressions,
+      nature: 'All'
     };
   },
-  _onNature: function() {
-      console.log('nature');
+  _onNature: function(natureObj) {
+    var nature = natureObj.value;
+    var exprs;
+    if (nature == 'All') {
+      exprs = this.state.expressions;
+    } else {
+      exprs = this.state.expressions.filter(function (exprObj) {
+              return exprObj.properties.nature == nature; });
+    }
+    this.setState({
+      nature: nature,
+      expressionsShown: exprs
+    });
   },
   render: function() {
     var options = [
         { value: 'All', label: 'Nature indifférente' },
         { value: 'Somme', label: 'Sommes' },
-        { value: 'Produit', label: 'Produits' }
+        { value: 'Diff', label: 'Différences' },
+        { value: 'Opposé', label: 'Opposés' },
+        { value: 'Produit', label: 'Produits' },
+        { value: 'Quotient', label: 'Quotients' },
+        { value: 'Inverse', label: 'Inverses' },
+        { value: 'Carré', label: 'Carrés' },
+        { value: 'Racine', label: 'Racines' },
+        { value: 'Puissance', label: 'Puissances' }
     ];
     return <div>
-    <Select name="form-field-name" value="All" options={options}
-            onChange={this._onNature}
+    <Select name="form-field-name"
+        options={options}
+        value={this.state.nature}
+        clearable={false}
+        onChange={this._onNature}
     />
     <ul>
-      {this.state.expressions.map(function(exprObj, idx){
+      {this.state.expressionsShown.map(function(exprObj, idx){
         return <li key={idx}>
-                  {idx+1} : <Expression expr={exprObj.expr} />
+                 {(idx<9?"  ":"") + (idx+1)} : <Expression expr={exprObj.expr} />
                </li>;
       })}
     </ul>
