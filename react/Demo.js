ba72e5b42688d60dc1b881edd5dcdebd2dba5356
Christophe Gragnic <cgragnic@netc.fr>
2017-01-17 18:45:43 +0100

    Fix whitespace

--- react/Demo.js
+++ react/Demo.js
@@ -15,22 +15,22 @@ module.exports = React.createClass({
       console.log('nature');
   },
   render: function() {
-      var options = [
-          { value: 'All', label: 'Nature indifférente' },
-          { value: 'Somme', label: 'Sommes' },
-          { value: 'Produit', label: 'Produits' }
-      ];
-      return <div>
-      <Select name="form-field-name" value="All" options={options}
-              onChange={this._onNature}
-      />
-      <ul>
-        {this.state.expressions.map(function(exprObj, idx){
-          return <li key={idx}>
-                    {idx+1} : <Expression expr={exprObj.expr} />
-                 </li>;
-        })}
-      </ul>
-      </div>;
+    var options = [
+        { value: 'All', label: 'Nature indifférente' },
+        { value: 'Somme', label: 'Sommes' },
+        { value: 'Produit', label: 'Produits' }
+    ];
+    return <div>
+    <Select name="form-field-name" value="All" options={options}
+            onChange={this._onNature}
+    />
+    <ul>
+      {this.state.expressions.map(function(exprObj, idx){
+        return <li key={idx}>
+                  {idx+1} : <Expression expr={exprObj.expr} />
+               </li>;
+      })}
+    </ul>
+    </div>;
   }
 });
